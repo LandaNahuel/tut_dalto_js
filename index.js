5908c0d3a4c9670a32f2182fccb49d44e20a1ff3
Nahuel Landa <landanahuel@icloud.com>
2023-02-14 18:33:41 -0300

    inicial

---
+++ index.js
@@ -0,0 +1,21 @@
+/*CONCATENADO
+numero1= "Nahuel Landa";
+numero2= 8;
+
+frase = `<h1>Soy ${numero1} y estoy codeando</h1>`
+
+document.write(frase)*/
+
+/*
+//COMPARACION
+let numero1= true;
+let numero2= true;
+
+let resultado = numero1 && numero2;
+let resultado1 = numero1 || numero2 ;
+let resultado3 =  !numero1;
+
+document.write(resultado1)*/
+
+//CONDICIONALES
+ 
